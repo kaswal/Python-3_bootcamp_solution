8a7e3e6198353bd43d1e3859ecd64d4b5ce25f70
kautilya aswal <er.kautilya21@gmail.com>
2019-12-11 18:03:56 -0800

    updated solution

--- Fizz buzz problem.py
+++ Fizz buzz problem.py
@@ -1,8 +1,8 @@
 # Write a program that prints the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number,
 #  and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
 
-x = list(range(1,101))
-for num in x:
+
+for num in range(1, 101):
     if num % 3 == 0 and num % 5 == 0:
         print('FizzBuzz')
     elif num % 3 == 0:
@@ -10,4 +10,4 @@ for num in x:
     elif num % 5 == 0:
         print('Buzz')
     else:
-        print(f'{num}')
+        print(num)
