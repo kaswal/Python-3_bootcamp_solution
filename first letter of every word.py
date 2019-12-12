5b0cbe3d0f7214a66fa6cf3d24e4b9d3f06b0938
kautilya aswal <er.kautilya21@gmail.com>
2019-12-11 18:07:39 -0800

    updated solution

--- first letter of every word.py
+++ first letter of every word.py
@@ -1,8 +1,7 @@
 # create a list of the first letters of every word in the string below:
 
 st = 'Create a list of the first letters of every word in this string'
-lst = st.split()
-new_lst=[]
-for word in lst:
+new_lst = []
+for word in st.split():
     new_lst.append(word[0])
 print(new_lst)
